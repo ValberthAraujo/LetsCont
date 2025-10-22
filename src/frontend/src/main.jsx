95b84c8248fb2492608fa4eb813cce09806c9329
ValberthAraujo <valberth10@gmail.com>
2025-10-22 13:21:20 +0000

    .

--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import ErrorBoundary from './components/common/ErrorBoundary.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <ErrorBoundary>
         <App />
-      </ErrorBoundary>
     </AuthProvider>
   </React.StrictMode>
 );
